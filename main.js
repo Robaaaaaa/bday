63c6f2c1ec8b370f90f23d4af8ef9f3278a1b860
Robaaa <linkmash030@gmail.com>
2024-05-07 16:17:59 +0300

    COMMIT

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ firstBtn.addEventListener("click",function(){
     let second= `
         <video src="high1.mp4" width ="150px" autoplay loop></video>
         <p>You're My best Friend I'll love you forever🫶</p>
-        <button id='second-btn'>Click2</button>`        
+        <button id='second-btn'>Next</button>`        
     firstMessage.innerHTML = second
 
 
@@ -15,9 +15,9 @@ firstBtn.addEventListener("click",function(){
     secondBtn.addEventListener("click",function(){
         second = `
             <img src='img.jpg' alt="spider x kitty" width='150px'>
-            <p>Issaiah💕 52:13-15❤ <br>
+            <p><span>Issaiah💕 52:13-15❤ </span><br>
             God loves you sm and has all the best for you plannned out. You can do this love, you're attracting all that you ever desired in this lifetime, you're healing, and you generate so much happiness in very room.
-            Remember, I love you okay? 🙂</p>
+            Remember, I love you okay? :)</p>
             <button id="third-btn">Previous</button>`
         firstMessage.innerHTML = second
 
