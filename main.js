251a22c92490bc22a428d65875af51c022462b2e
Robaaaaaa <162624596+Robaaaaaa@users.noreply.github.com>
2024-05-07 18:10:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ firstBtn.addEventListener("click",function(){
     secondBtn.addEventListener("click",function(){
         second = `
             <img src='img.jpg' alt="spider x kitty" width='150px'>
-            <p><span>Issaiah💕 52:13-15❤ </span><br>
-            God loves you sm and has all the best for you plannned out. You can do this love, you're attracting all that you ever desired in this lifetime, you're healing, and you generate so much happiness in very room.
+            <p><span>Isaiah💕 52:13-15❤ </span><br>
+            God loves you sm and has all the best for you planned out. You can do this love, you're attracting all that you ever desired in this lifetime, you're healing, and you generate so much happiness in very room.
             Remember, I love you okay? :)</p>
             <button id="third-btn">Previous</button>`
         firstMessage.innerHTML = second
